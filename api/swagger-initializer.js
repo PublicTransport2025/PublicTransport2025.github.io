cbe1d65c374bdc6b41169141e31fee20d7ec1b79
Kiryanov_Aleksey <alexkir04@inbox.ru>
2025-03-29 22:01:43 +0300

    dosc(TP-72): Added swagger core

---
+++ api/swagger-initializer.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://raw.githubusercontent.com/PublicTransport2025/Docs/refs/heads/main/%D0%9F%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F/openapi.yaml",
+    dom_id: '#swagger-ui',
+    deepLinking: false,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout",
+showExtensions: false,
+    showCommonExtensions: false,
+  });
+
+  //</editor-fold>
+};
